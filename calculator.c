1d12527ebeaec57eea30ba1254c517cef8847ac2
Soudamini <soudamini_patil@yahoo.com>
2016-08-08 16:54:27 +0530

    Update calculator.c
    Included add() function

--- calculator.c
+++ calculator.c
@@ -5,4 +5,11 @@ void main()
 {
   int a, b, c;
   // Add calculator functions here 
+  // Add functionalilits like addition subtraction etc.
+  
+  int add (int p, int q)
+  {
+    return a+b;
+    
+  }
 }
